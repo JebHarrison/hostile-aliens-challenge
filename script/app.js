d8a67ea35375bf345610129b4d0468a948b669e8
James Harrison <jamesebharrison@hotmail.com>
2022-03-04 16:53:52 +0000

    styled shooter in the center of the grid

--- script/app.js
+++ script/app.js
@@ -1,4 +1,5 @@
 const grid = document.querySelector('.grid')
+let currentShooterIndex = 202
 
 for (let i = 0; i < 225; i++) {
     const square = document.createElement('div')
@@ -10,7 +11,7 @@ const squares = Array.from(document.querySelectorAll('.grid div'))
 const hostileAliens = [
     0,1,2,3,4,5,6,7,8,9,
     15,16,17,18,19,20,21,22,23,24,
-    30,31,32,33,34,35,36,37,38,39,
+    30,31,32,33,34,35,36,37,38,39
 ]
 
 function draw() {
@@ -21,4 +22,4 @@ function draw() {
 
 draw()
 
-squares[200].classList.add('shooter')
+squares[currentShooterIndex].classList.add('shooter')
