fe08f02e8baae21fdf5ab4b5a86ab937fd3117d1
James Harrison <jamesebharrison@hotmail.com>
2022-03-04 16:12:30 +0000

    changed function to hostile aliens and class to hostile

--- script/app.js
+++ script/app.js
@@ -7,14 +7,14 @@ for (let i = 0; i < 225; i++) {
 
 const squares = Array.from(document.querySelectorAll('.grid div'))
 
-const alienInvaders = [
+const hostileAliens = [
     0,1,2,3,4,5,6,7,8,9,
     15,16,17,18,19,20,21,22,23,24,
     30,31,32,33,34,35,36,37,38,39,
 ]
 
 function draw() {
-    for (let i = 0; i < alienInvaders.length; i++) {
-        squares[alienInvaders[i]].classList.add('invader')
+    for (let i = 0; i < hostileAliens.length; i++) {
+        squares[hostileAliens[i]].classList.add('hostile')
     }
 }
