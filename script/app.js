59159ce720bfb44c8af838739645dbdabc768a0d
James Harrison <jamesebharrison@hotmail.com>
2022-03-04 16:10:47 +0000

    added styling color for alien invader class

--- script/app.js
+++ script/app.js
@@ -12,3 +12,9 @@ const alienInvaders = [
     15,16,17,18,19,20,21,22,23,24,
     30,31,32,33,34,35,36,37,38,39,
 ]
+
+function draw() {
+    for (let i = 0; i < alienInvaders.length; i++) {
+        squares[alienInvaders[i]].classList.add('invader')
+    }
+}
